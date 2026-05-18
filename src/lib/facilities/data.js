25517aab6f67c2d1470eb8645e480f960c361c27
Zihad-1883 <zihad.pilot18@gmail.com>
2026-05-18 19:01:02 +0600

    created env file to track the api link

--- src/lib/facilities/data.js
+++ src/lib/facilities/data.js
@@ -1,11 +1,11 @@
 export const fetchFacilities = async () => {
-    const res = await fetch('http://localhost:8080/all-facilities');
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/all-facilities`);
     const data = await res.json();
     return data;
 }
 
 export const fetchFeaturedFacilities = async () => {
-    const res = await fetch('http://localhost:8080/features');
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/features`);
     const data = await res.json();
     return data;
 }
