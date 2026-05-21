9538713b90b3771f31ce0043d37c32f88e6a67b3
Zihad-1883 <zihad.pilot18@gmail.com>
2026-05-22 03:25:41 +0600

    fixing auth issues

--- src/proxy.js
+++ src/proxy.js
@@ -1,9 +1,16 @@
 import { NextResponse } from 'next/server'
 
 export function proxy(request) {
+  const cookieStore = request.cookies
+  
+  // log all cookies to see exact names
+  const allCookies = cookieStore.getAll()
+  console.log("All cookies:", allCookies.map(c => c.name))
+
   const sessionToken = 
-    request.cookies.get('better-auth.session_token')?.value ||
-    request.cookies.get('__Secure-better-auth.session_token')?.value
+    cookieStore.get('better-auth.session_token')?.value ||
+    cookieStore.get('__Secure-better-auth.session_token')?.value ||
+    cookieStore.get('better-auth_session_token')?.value
 
   if (!sessionToken) {
     const loginUrl = new URL('/login', request.url)
@@ -19,6 +26,6 @@ export const config = {
     '/my-bookings',
     '/add-facility', 
     '/manage-facilities',
-    '/all-facilities/:path',
+    '/all-facilities/(.+)',
   ],
 }
